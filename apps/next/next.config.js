5db5da2d43ba5188047cabf50535007285bbf73a
AhmedKamalOrabi <ahmed.orabi@talabat.com>
2022-03-25 16:47:35 +0200

    remove next project

--- apps/next/next.config.js
+++
@@ -1,9 +0,0 @@
-// ? https://nextjs.org/docs/api-reference/next.config.js/introduction
-
-/**
- * @type {import('next').NextConfig}
- */
-module.exports = {
-  poweredByHeader: false,
-  swcMinify: true,
-};
