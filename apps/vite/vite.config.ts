76a03b74f26b5d89667e076f744521000a93350e
AhmedKamalOrabi <ahmed.orabi@talabat.com>
2022-03-30 06:46:32 +0200

    fix(vite-config): remove empty object fot react plugin

--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -1,14 +1,16 @@
 import path from 'node:path';
 
-import reactPlugin from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { ViteWebfontDownload as webfontPlugin } from 'vite-plugin-webfont-dl';
+import { ViteWebfontDownload } from 'vite-plugin-webfont-dl';
 
 // ? https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    reactPlugin(),
-    webfontPlugin(['https://www.google.com/search?q=google%20fonts%20roboto']),
+    react({ jsxRuntime: 'classic' }),
+    ViteWebfontDownload([
+      'https://www.google.com/search?q=google%20fonts%20roboto',
+    ]),
   ],
   resolve: {
     alias: {
